dbb882d154a37acc00b2e5497a814dc5840d5062
Amar Bhamra <Amar.Bhamra@zion.local>
2017-02-02 16:52:49 +0000

    fix hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ module.exports = {
     entry: './src/js/app.js',
     output: {
         path: './dist',
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/dist/', // instead of publicPath: '/build/' 
     },
     module: {
         loaders: [{
             test: /\.js$/,
             exclude: /node_modules/,
-            loader: 'babel-loader',
+            loader: "babel-loader"
         }]
     }
 }
